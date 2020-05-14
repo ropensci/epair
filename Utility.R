561150976f6c9ccb2ee55d9a4c95eb0621b990ad
GLOrozcoM <glorozcom@protonmail.com>
2020-05-14 15:49:54 -0700

    Wrote functions to simplify table scraping from API AQS site.

--- Utility.R
+++ Utility.R
@@ -17,3 +17,25 @@ query <- function( base_query, authentication  ){
   call <- paste( base_query, authentication, sep = '' )
   return( call )
 }
+
+# Output a data frame containing a table using url and xpath to the table 
+get.table <- function( url, table.xpath ){
+  found.table <- url %>%
+    read_html() %>%
+    html_nodes( xpath = table.xpath ) %>% 
+    html_table()
+  return( found.table[[1]] )
+}
+
+# Remove \r \n \t from entries
+# TODO, simplify code with multiple pattern regex matching
+remove.escapes <- function( df ){
+  for(i in 1:nrow(df) ){
+    for(j in 1:ncol(df) ){
+      df[i, j] <- gsub( "\n", "", df[i, j] )
+      df[i, j] <- gsub( "\r", "", df[i, j] )
+      df[i, j] <- gsub( "\t", "", df[i, j] )
+    }
+  }
+  return( df )
+}
--- explorehtml.R
+++ explorehtml.R
@@ -1,50 +1,16 @@
 # Get html data for API how to site https://aqs.epa.gov/aqsweb/documents/data_api.html
 
-library(XML)
-
-doc.html = htmlTreeParse('https://aqs.epa.gov/aqsweb/documents/data_api',
-                         useInternal = TRUE)
-
 library(rvest)
+
+source("Utility.R")
+
+# Get table showing list of services
 url <- "https://aqs.epa.gov/aqsweb/documents/data_api.html"
-raw.html.doc <- html( url )
-raw.html.doc
-class(raw.html.doc)
-
 table.path <- '//*[@id="main-content"]/div[2]/div[1]/div/div/table[1]'
+df <- get.table( url, table.path )
+class(df)
+head(df, 1)
+df <- remove.escapes( df )
+df
 
-node <- html_nodes( raw.html.doc, xpath = table.path )
 
-the.table <- html_table( node )
-
-class(the.table[[1]])
-
-head(the.table[[1]])
-
-final.table <- the.table[[1]]
-
-summary(final.table)
-final.table[1,1]
-final.table[1,]
-
-final.table[3,]
-class(final.table[3,2])
-
-> foo <- vector(mode="list", length=3)
-> names(foo) <- c("tic", "tac", "toe")
-> foo[[1]] <- 12; foo[[2]] <- 22; foo[[3]] <- 33
-
-table.list <- vector( mode = "list", length = nrow( final.table) )
-names( table.list ) <- as.vector( final.table[1])
-table.list
-
-names.vector <- c()
-for( i in 1:nrow( final.table ) ){ 
-  names.vector <- c( names.vector, final.table[i,1])
-}
-names.vector
-
-names( table.list ) <- names.vector
-table.list
-
-t(final.table)
