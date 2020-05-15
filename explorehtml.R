af621cb22ecd50846b159e4fa5b6ec8fd1f1f24c
GLOrozcoM <glorozcom@protonmail.com>
2020-05-15 11:12:36 -0700

    Experimenting with getting links from tables.

--- explorehtml.R
+++ explorehtml.R
@@ -1,16 +1,23 @@
 # Get html data for API how to site https://aqs.epa.gov/aqsweb/documents/data_api.html
-
 library(rvest)
-
 source("Utility.R")
 
-# Get table showing list of services
 url <- "https://aqs.epa.gov/aqsweb/documents/data_api.html"
-table.path <- '//*[@id="main-content"]/div[2]/div[1]/div/div/table[1]'
-df <- get.table( url, table.path )
-class(df)
-head(df, 1)
-df <- remove.escapes( df )
-df
+table.xpath <- '//*[@id="main-content"]/div[2]/div[1]/div/div/table[1]'
+get.table <- function( url, table.xpath ){
+  found.table <- url %>%
+    read_html() %>%
+    html_nodes( xpath = table.xpath ) %>% 
+    html_table()
+  return( found.table[[1]] )
+}
+
+
+found.table <- url %>%
+  read_html() %>%
+  html_nodes( xpath = table.xpath) %>%
+  html_attrs()
+  
+found.table
 
 
