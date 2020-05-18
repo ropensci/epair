b8bc11fd4e096f7a75924e9edff5f1adaf396be5
GLOrozcoM <glorozcom@protonmail.com>
2020-05-18 14:39:16 -0700

    Started query production for data in paper.

---
+++ GetDataForPaper.R
@@ -0,0 +1,8 @@
+# Get exact data used in published paper
+
+
+# CT is 09
+variables <- list( 'bdate' = '20160706',
+                   'edate' = '20160805',
+                   'state' = '09',
+                   'param' = ) 
