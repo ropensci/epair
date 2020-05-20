16c6679b386e407e89f11d35ce8ebd79834a95dd
GLOrozcoM <glorozcom@protonmail.com>
2020-05-20 16:59:23 -0700

    Mock up of transformation of a table into a list structure - makes selection easier.

--- GetAllTables.R
+++ GetAllTables.R
@@ -16,6 +16,83 @@ list.tbls <- html_table(tbls, fill = TRUE)
 sample.table <- list.tbls[[15]]
 sample.table$`Required Variables`
 
+
+
+
+
+
+
+
+### Create structure, formulate structure for endpoints and querying.
+# -> Makes sense for this to be non-rectangular
+
+sample.table$Service[1]
+
+list.data <- list()
+# 1 Collapse service 
+list.data$Service <- sample.table$Service[1]
+
+# Filter - Endpoint - Example
+# -- should be rectangular. or an arrow somehow...
+
+sample.table[3,]
+
+nrow(sample.table)
+
+for( i in 1:nrow( sample.table )){
+  print( sample.table[i, 4] )
+}
+
+
+sample.table
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 2 If an entry matches the service then it doesn't belong. Turn it into an NA. 
+# Or remove it. Contain only the filters. 
+## -- Thought is the row on left expands into rows on right.
+## --> Remove those
+
+# Turn into a list...
+list.data$Filter <- sample.table$Filter[which( sample.table$Filter != list.data$Service  )]
+list.data$Filter
+
+list.data
+
+# 3 Find wat to match the filter with the proper endpoint
+
+# 4 Divide up the examples to be in a separate node
+sample.table$Endpoint
+
+# It seems like optional and required variables need their own filtering mechanism.
+
+sample.table$`Optional Variables`
+
+
+
+
+
+
+
+
+
 ######## Just endpoints #########
 
 ### Find the endpoints
