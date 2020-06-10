c627b05b0b3d186f8592923c3ca1864779302b4c
GLOrozcoM <glorozcom@protonmail.com>
2020-06-10 06:30:56 -0700

    First improvement to list structure maker.

--- DevelopTableToListFunction.R
+++ DevelopTableToListFunction.R
@@ -9,7 +9,7 @@ tbls[[5]]$Endpoint
 # Split into tables having multiple examples and those having only one
 # --> Anyone before the 4th is unnecessary
 # - Meta data is the only table having the third-ish entry down as multiple
-tables.to.modify <- list( tbls[[4]],
+tables.to.modify <- list( tbls[[4]],tbls[[5]],
                                  tbls[[6]],
                                  tbls[[7]],
                                  tbls[[8]],
@@ -25,9 +25,6 @@ tables.to.modify <- list( tbls[[4]],
 
 # Prototype the list
 single <- tbls[[4]]
-
-# -- service.list -> final structure containing all services and their info
-service.list <- list()
 
 #' Give a service name
 #'
@@ -53,6 +50,7 @@ service.list <- assign.service.name( single, service.list)
 #'
 #' @examples
 get.true.filters <- function( df ){
+  service.name <- df$Service[1]
   unique.filters <- unique( df$Filter )
   unique.filters <- unique.filters[which( unique.filters != service.name )]
   return( unique.filters )
@@ -91,23 +89,103 @@ get.first.occurences <- function( df ){
 }
 first.occurences <- get.first.occurences( single )
 
-# Holds indices of first occurence of a filter
-# -- hypothesizing they fit in properly
-first.indices.for.filters 
+# Possible to extract something here...
 
+names(first.occurences)[1]
+# "Is the API available for use?"
 
+#' Title
+#'
+#' @param i 
+#' @param df 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+generate.filter.content <- function( i, df){
+  filter.content <- list( Endpoint = df$Endpoint[i],
+                          RequiredVariables = df$`Required Variables`[i],
+                          OptionalVariables = df$`Optional Variables`[i],
+                          Example = df$Endpoint[i + 1])
+  return( filter.content )
+}
+rev.his <- generate.filter.content( 3, single)
 
+#' Title
+#'
+#' @param filter.name 
+#' @param i 
+#' @param df 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+setup.single.filter <- function( filter.name, i, df ){
+  filter.content <- generate.filter.content( i, df )
+  filter.out <- list()
+  filter.out[[filter.name]] <- filter.content
+  return( filter.out )
+}
 
+# Get each filter in that table named and ready to go 
+filters.list <- list()
+first.occurences <- get.first.occurences( single )
+n = length(first.occurences)
+for( i in 1:n){
+  filter.name <- names(first.occurences)[i]
+  filter.index <- first.occurences[[i]]
+  filter.out <- setup.single.filter( filter.name, filter.index, single) # Single to modify for many
+  filters.list <- c( filters.list, filter.out)
+}
+filters.list$`Known Issues`
 
-######
+#' Get filters going
+#'
+#' @param df 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+generate.filters.list <- function( df ){
+  filters.list <- list()
+  first.occurences <- get.first.occurences( df )
+  n = length(first.occurences)
+  for( i in 1:n){
+    filter.name <- names(first.occurences)[i]
+    filter.index <- first.occurences[[i]]
+    filter.out <- setup.single.filter( filter.name, filter.index, df)
+    filters.list <- c( filters.list, filter.out)
+  }
+  return( filters.list )
+}
+single.filters <- generate.filters.list( single )
 
-# Then take the min
-single$Filter
+####### Putting it all together
 
-single$Filters 
+#' Title
+#'
+#' @param df 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+setup.service <- function( df ){
+  service.list <- list()
+  service.name <- df$Service[1]
+  service.list[[service.name]] <- generate.filters.list( df )
+  return(service.list)
+}
+example <- setup.service(single)
 
-single$Endpoint
-
-single$`Required Variables`
-
-single$`Optional Variables`
+# -- service.list -> final structure containing all services and their info
+service.list <- list()
+for( i in 1:length(tables.to.modify)){
+  df <- tables.to.modify[[i]]
+  single.service <- setup.service( df )
+  service.list <- c( service.list, single.service)
+}
+service.list$List$`Parameters in a class (obtain the list of classes from the List - Parameter Classes service)`
