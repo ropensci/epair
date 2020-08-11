f0b4d04442158ec8aac2f01826fe61a0e0e0abe5
GLOrozcoM <glorozcom@protonmail.com>
2020-08-11 15:23:57 -0700

    Make test for handling spaces in variables

--- tests/testthat/test_call_construction.R
+++ tests/testthat/test_call_construction.R
@@ -36,3 +36,10 @@ test_that("Multiple variables added to query properly", {
   expect_equal(found_call, exp_call)
 })
 
+test_that("Variables containing spaces are accepted into a potential call", {
+  base <- "http//my_website/email=myEmail&key=myKey/endpoint?"
+  vars <- list("state" = "10", "param_with_space" = "param aqi")
+  found <- epair::add.variables(base, vars)
+  exp <- "http//my_website/email=myEmail&key=myKey/endpoint?&state=10&param_with_space=param%20aqi"
+  expect_equal(found, exp)
+})
