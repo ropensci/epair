61037e5b1d262bb31675d29c49a6bb7f12690d54
GLOrozcoM <glorozcom@protonmail.com>
2021-10-31 10:54:39 +0200

    Working redactor and requester setup.

--- inst/httptest/redact.R
+++ inst/httptest/redact.R
@@ -1,6 +1,7 @@
 credential.redactor <- function (response) {
   require(magrittr, quietly=TRUE)
     response %>%
+      gsub_response("https://aqs.epa.gov/data/api/", "api") %>%
       gsub_response(Sys.getenv("aqs_email"), "fake_aqs_email@domain.com") %>%
       gsub_response(Sys.getenv("aqs_api_key"), "fake_aqs_api_key")
 }
