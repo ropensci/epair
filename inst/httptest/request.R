c414fa1657091f3aebf5efbdc66cb108ded4d49a
GLOrozcoM <glorozcom@protonmail.com>
2021-12-20 16:29:52 -0700

    Fix long file path in qaAnnualPerformanceEvaluations

--- inst/httptest/request.R
+++ inst/httptest/request.R
@@ -2,6 +2,7 @@ request.sanitizer <- function (request) {
   require(magrittr, quietly=TRUE)
   request %>%
     gsub_request("https\\://aqs.epa.gov/data/api/", "") %>%
+    gsub_request("qaAnnualPerformanceEvaluations", "qaAPE") %>%
     gsub_request(Sys.getenv("aqs_email"), "fake_aqs_email@domain.com") %>%
     gsub_request(Sys.getenv("aqs_api_key"), "fake_aqs_api_key")
 }
